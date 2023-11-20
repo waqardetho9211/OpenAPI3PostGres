02972a2bd95fb08ca1fa65cca768f20d9ac1bdc6
Waqar Detho <waqar.detho@gmail.com>
2023-11-20 21:58:20 +0100

    Add logging in the project

--- src/main/java/com/exampleHotel/demo/DemoApplication.java
+++ src/main/java/com/exampleHotel/demo/DemoApplication.java
@@ -1,12 +1,18 @@
 package com.exampleHotel.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("this is a info message");
+		logger.warn("this is a warn message");
+		logger.error("this is a error message");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
